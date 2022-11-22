ea7feb02dd1332610f58e1d91931e8b9dc7b7dfa
Josh Lee <joshlee@usc.edu>
2022-11-21 23:57:11 -0800

    Finish Testing

--- app/src/androidTest/java/com/example/villageplanner/ReminderTests/ReminderPageUITests.java
+++ app/src/androidTest/java/com/example/villageplanner/ReminderTests/ReminderPageUITests.java
@@ -2,10 +2,14 @@ package com.example.villageplanner.ReminderTests;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 import static com.example.villageplanner.ReminderTests.RecyclerMatcher.numReminders;
 
@@ -17,9 +21,11 @@ import androidx.test.espresso.action.GeneralSwipeAction;
 import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Swipe;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.uiautomator.UiDevice;
 
 import com.example.villageplanner.R;
 import com.example.villageplanner.ReminderLogic.ReminderPage;
@@ -38,27 +44,29 @@ public class ReminderPageUITests {
 
     @Test
     public void testDeleteReminder() {
-        onView(withId(R.id.recyclers)).perform(
-                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
-                        Swipe.SLOW, GeneralLocation.BOTTOM_RIGHT, GeneralLocation.BOTTOM_LEFT,
-                        Press.FINGER)));
+//        onView(withId(R.id.recyclers)).perform(
+//                RecyclerViewActions.actionOnItemAtPosition(50, new GeneralSwipeAction(
+//                        Swipe.SLOW, GeneralLocation.BOTTOM_RIGHT, GeneralLocation.BOTTOM_LEFT,
+//                        Press.FINGER)));
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        device.swipe(900,300,400,300,10);
         onView(withId(android.R.id.content)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testLikeReminder() {
-        onView(withId(R.id.recyclers)).perform(
-                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
-                        Swipe.SLOW, GeneralLocation.BOTTOM_LEFT, GeneralLocation.BOTTOM_RIGHT,
-                        Press.FINGER)));
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        device.swipe(400,300,500,300,10);
         onView(withId(android.R.id.content)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void testWhenReminderClickedGoToRemiderViewPage() {
+    public void testWhenReminderClickedGoToRemiderViewPage() throws InterruptedException {
         onView(withId(R.id.recyclers)).perform(
                 RecyclerViewActions.actionOnItemAtPosition(0,  click()));
-        onView(withId(R.id.view_reminder)).check(matches(isDisplayed()));
+
+        // Can I click a button that appears in the reminder view page?
+        onView(withId(R.id.editReminder)).perform(click());
     }
 
     @Test
@@ -68,5 +76,22 @@ public class ReminderPageUITests {
         onView(withId(R.id.view_reminder)).check(matches(isDisplayed()));
         onView(withId(R.id.editReminder)).perform(click());
         onView(withId(R.id.create_the_reminder)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNoEmptyReminder() {
+        onView(withId(R.id.add_remind)).perform(click());
+        onView(withId(R.id.submitReminder)).perform(click());
+        onView(withId(R.id.submitReminder)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNoEditIntoEmpty() {
+        onView(withId(R.id.recyclers)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0,  click()));
+        onView(withId(R.id.editReminder)).perform(click());
+        onView(withId(R.id.reminderTitle)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.submitReminder)).perform(click());
+        onView(withId(R.id.submitReminder)).check(matches(isDisplayed()));
     }
 }
